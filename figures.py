0d993f847662b482656d689f009d17c74610c1c4
yardenas <yardas@student.ethz.ch>
2022-03-09 19:51:28 +0100

    Add timeseries

--- figures.py
+++ figures.py
@@ -1,10 +1,10 @@
 import plotly.express as px
-from dash import dcc
+from dash import dcc, html
 
 
 def plan_vs_actual_fig(budget):
   return dcc.Graph(
-      id='plan_vs_actual_all_budgetary_items',
+      id='plan-vs-actual-all-budgetary-items',
       figure=px.bar(budget, x='items', y='amounts'),
       className='figure')
 
@@ -12,5 +12,16 @@ def plan_vs_actual_fig(budget):
 def piechart(names, values):
   return dcc.Graph(
       id='piechart',
-      figure=px.pie(names=names, values=values, hole=.3),
+      figure=px.pie(names=names, values=values, hole=.9),
       className='figure')
+
+
+def plan_vs_actual_time_series(budget, actual):
+  if budget.empty or actual.empty:
+    return html.P('No Data', id='no-data-error')
+  return dcc.Graph(
+      id='plan-vs-actual-time',
+      figure=px.line(
+          x=actual.index,
+          y=actual['amount'].rolling('1D').sum() / budget['amounts'].sum()),
+  )
